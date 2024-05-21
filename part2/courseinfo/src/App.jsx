3c6188d5f79d0d558f05d4870279889cd44c81d3
Dyadyusha Artem <artikool5@yandex.ru>
2024-05-21 13:03:57 +0300

    exc: 2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
 function Header(props) {
-  return <h1>{props.course}</h1>;
+  return <h2>{props.course}</h2>;
 }
 
 function Part(props) {
@@ -29,25 +29,50 @@ function Total({ parts }) {
 
 function Course({ course }) {
   return (
-    <>
+    <section>
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
+    </section>
+  );
+}
+
+function Courses({ courses }) {
+  return (
+    <>
+      {courses.map((course) => {
+        return <Course key={course.id} course={course} />;
+      })}
     </>
   );
 }
 
 function App() {
-  const course = {
-    id: 1,
-    name: "Half stack application development",
-    parts: [
-      { name: "Fundamentals of React", excercises: 10, id: 1 },
-      { name: "Using props to pass data", excercises: 7, id: 2 },
-      { name: "State of a component", excercises: 14, id: 3 },
-    ],
-  };
-  return <Course course={course} />;
+  const courses = [
+    {
+      id: 1,
+      name: "Half stack application development",
+      parts: [
+        { name: "Fundamentals of React", excercises: 10, id: 1 },
+        { name: "Using props to pass data", excercises: 7, id: 2 },
+        { name: "State of a component", excercises: 14, id: 3 },
+      ],
+    },
+    {
+      id: 2,
+      name: "Node.js",
+      parts: [
+        { name: "Routing", excercises: 4, id: 1 },
+        { name: "Middleware", excercises: 7, id: 2 },
+      ],
+    },
+  ];
+  return (
+    <>
+      <h1>Web development curriculum</h1>
+      <Courses courses={courses} />
+    </>
+  );
 }
 
 export default App;
