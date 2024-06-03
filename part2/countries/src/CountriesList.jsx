d7e1a12900865258345b7d4a9dda649217e09aa3
Dyadyusha Artem <artikool5@yandex.ru>
2024-06-03 18:59:07 +0300

    exc: 2.19

--- part2/countries/src/CountriesList.jsx
+++ part2/countries/src/CountriesList.jsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
+import { CountryInfo } from "./CountryInfo";
+
 export function CountriesList({ filteredCountries }) {
+  const [openedCountries, setOpenedCountries] = useState([]);
+
+  const handleOpenedCountries = (id) => {
+    if (openedCountries.includes(id)) {
+      const newCountries = openedCountries.filter((code) => code !== id);
+      setOpenedCountries(newCountries);
+      return;
+    }
+
+    setOpenedCountries(openedCountries.concat(id));
+  };
+
   return (
     <div>
       {filteredCountries.map((country) => {
-        return <p key={country.ccn3}>{country.name.common}</p>;
+        return (
+          <>
+            <p key={country.ccn3}>
+              {country.name.common}
+              <button
+                onClick={() => handleOpenedCountries(country.ccn3)}
+                type="button"
+              >
+                Show
+              </button>
+            </p>
+            {openedCountries.includes(country.ccn3) ? (
+              <CountryInfo country={country} />
+            ) : null}
+          </>
+        );
       })}
     </div>
   );
